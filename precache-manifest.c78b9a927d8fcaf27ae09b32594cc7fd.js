6a1d191450207cc63dc2c0461efdb99fdc1fd79e
Gladstone Rose <gladstonerose@hotmail.com>
2024-03-28 09:13:25 -0500

    Updates

---
+++ precache-manifest.c78b9a927d8fcaf27ae09b32594cc7fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29602f4805b813cdae9fe4957718a3d2",
+    "url": "/learn/index.html"
+  },
+  {
+    "revision": "95d04c91666b236f485a",
+    "url": "/learn/static/js/2.8696a317.chunk.js"
+  },
+  {
+    "revision": "66a970c095f8131a254a",
+    "url": "/learn/static/js/main.3d0363a8.chunk.js"
+  },
+  {
+    "revision": "61853bc14cebb70626f7",
+    "url": "/learn/static/js/runtime~main.87ca8553.js"
+  }
+]);
